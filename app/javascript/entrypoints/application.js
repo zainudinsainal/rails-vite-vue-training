be4c7f4f3adf4797f9fff565f7a5c4597ad79bfe
zainudinsainal <supernova37@gmail.com>
2021-12-02 14:36:54 +0800

    Intgrate vue with rails using vite

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -28,3 +28,8 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+import initVueApp from "../src/main.js"
+
+initVueApp()
+
